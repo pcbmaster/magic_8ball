738897dbfda70b9e01e8bf275f3fa403be22a512
parkerprecisionweb <dallasjamesparker@gmail.com>
2018-08-16 15:22:37 -0600

    add methods add_answers/reset_answers & main.rb methods

--- 8ball.rb
+++ 8ball.rb
@@ -1,11 +1,23 @@
 #Have a constant array of default answers
-#Have functions:
-#   *add_answers
-#   *reset_answers
-#   *print_answers
 
+#Copies in answers from Contant array, and works as a container to accept new questions/answers
+
+
+#####Have methods:
+#   *print_menu
+#   *add_answers
+#   *print_answers
 #should getting the user's question be part of this class?
 
 class EightBall
-
+  def add_answers
+    puts 'Please enter a NEW answer to be used.'
+    @added_answer = gets.stip
+  end
+  
+  #   *reset_answers
+  def reset_answers
+    puts 'Are you sure you would like to reset the answer bank? (y/n)'
+    @reset_answer_confirm = gets.stip
+  end
 end
---
+++ main.rb
@@ -0,0 +1,12 @@
+def display_initial_menu
+  puts 'Type a question to get an answer or type QUIT to end the program:'
+end
+
+def display_in_game_menu
+  puts 'Aske me another question!'
+  puts 'Type a question to get an answer or type QUIT to end the program:'
+end
+
+def display_quit_screen
+  puts 'Thanks for playing Magic 8 Ball!!!'
+end
