2b66a9e5b6d8f0aa2cd2fba30625e4ecd5eb5980
Brett Monty <pcbmaster@github.com>
2018-08-16 14:54:58 -0600

    added skeleton 8ball.rb

---
+++ 8ball.rb
@@ -0,0 +1,11 @@
+#Have a constant array of default answers
+#Have functions:
+#   *add_answers
+#   *reset_answers
+#   *print_answers
+
+#should getting the user's question be part of this class?
+
+class EightBall
+
+end
